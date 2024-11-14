d238290c90866884921a3aa6f631abdfcd06c7ba
shubham kumar <85861742+Shubhamthakur321@users.noreply.github.com>
2024-11-14 17:19:20 +0530

    Graph Analysis

--- data_analysis.py
+++ data_analysis.py
@@ -6,7 +6,22 @@ import matploatlib.pyplot as plt
 df = pd.read_csv('C:/Users/shubham_kumar/Downloads/Expanded_data_with_more_features.csv/student_data.csv')
 #Check some values present in column.
 print(df.head())
+
 # Check the statatics numerical values column.
 df.describe()
+
 # Information of all the column and its data type
 df.info()
+
+#Check the null values and its count of all the column
+df.isnull().sum()
+
+# Bar grap of the Male and Female count
+df=df.drop('Unnamed: 0',axis=1)
+plt.figure(figsize=(8,5))
+ax=sns.countplot(data=df,x='Gender')
+ax.bar_label(ax.containers[0])
+plt.show()
+
+# From the above chat we have analysis that female count is more then male count.
+
