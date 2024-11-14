a1732babf412fc8cb36382e8cd5e43ed4528e1bf
shubham kumar <85861742+Shubhamthakur321@users.noreply.github.com>
2024-11-14 14:25:35 +0530

    Check the data and trf the data

--- data_analysis.py
+++ data_analysis.py
@@ -2,3 +2,11 @@ import pandas as pd
 import numpy as numpy
 import seaborn as sns
 import matploatlib.pyplot as plt
+#Import file from source path
+df = pd.read_csv('C:/Users/shubham_kumar/Downloads/Expanded_data_with_more_features.csv/student_data.csv')
+#Check some values present in column.
+print(df.head())
+# Check the statatics numerical values column.
+df.describe()
+# Information of all the column and its data type
+df.info()
